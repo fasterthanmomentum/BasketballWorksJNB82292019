8800c1d8262b085e0c53644ed3c43267e49ca4a0
Joy Best <fasterthanmomentum@gmail.com>
2019-08-29 21:52:59 -0400

    Initial Commit

---
+++ BasketballWorksJNB82292019Tests/BasketballWorksJNB82292019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BasketballWorksJNB82292019Tests.swift
+//  BasketballWorksJNB82292019Tests
+//
+//  Created by JOY BEST on 8/29/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+import XCTest
+@testable import BasketballWorksJNB82292019
+
+class BasketballWorksJNB82292019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
