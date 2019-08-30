600580b140ee89a22d34334ad709ccd8588f4056
Joy Best <fasterthanmomentum@gmail.com>
2019-08-30 09:23:18 -0400

    i think the app finally works

--- BasketballWorksJNB82292019/ViewController.swift
+++ BasketballWorksJNB82292019/ViewController.swift
@@ -13,6 +13,12 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var hoopAdded = false
+    
+    @IBOutlet var screenTapped: UITapGestureRecognizer!
+    
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +28,89 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
-        // Create a new scene
+                // Create a new scene
         let scene = SCNScene(named: "art.scnassets/hoop.scn")!
-        
+       
         // Set the scene to the view
        sceneView.scene = scene
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+
     }
+    
+    
+    
+    
+    
+    
+
+
+        
+        
+        
+    
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+    
+    if !hoopAdded {
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: [.existingPlaneUsingExtent])
+        if let result = hitTestResult.first {
+            addHoop(result: result)
+            hoopAdded = true
+        }
+        } else {
+        createBasketball()
+        }
+    }
+    
+    
+   
+    func addHoop(result: ARHitTestResult) {
+        let hoopScene = SCNScene(named: "art.scnassets/hoop.scn" )
+        guard let hoopNode = hoopScene?.rootNode.childNode(withName:
+            "Hoop", recursively: false) else { return}
+        let planePosition = result.worldTransform.columns.3
+        hoopNode.position = SCNVector3(planePosition.x, planePosition.y, planePosition.z)
+        hoopNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: hoopNode, options:
+            [SCNPhysicsShape.Option.type:
+            SCNPhysicsShape.ShapeType.concavePolyhedron]))
+        sceneView.scene.rootNode.addChildNode(hoopNode)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func createBasketball() {
+        guard let currentFrame = sceneView.session.currentFrame else
+        { return }
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.25))
+        ball.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+        let cameraTransform = SCNMatrix4(currentFrame.camera.transform)
+        ball.transform = cameraTransform
+        let physicsBody = SCNPhysicsBody(type: .dynamic, shape:
+            SCNPhysicsShape(node: ball, options:
+                [SCNPhysicsShape.Option.collisionMargin: 0.01]))
+        ball.physicsBody = physicsBody
+        let power = Float(10.0)
+        let force = SCNVector3(-cameraTransform.m31*power, -cameraTransform.m32*power, -cameraTransform.m33*power)
+        ball.physicsBody?.applyForce(force, asImpulse: true)
+    
+        sceneView.scene.rootNode.addChildNode(ball)
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,7 +118,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = .vertical
         // Run the view's session
         sceneView.session.run(configuration)
     }
